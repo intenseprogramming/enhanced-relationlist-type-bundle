6347cd2d2d36423624ca5a76e027e1a557a44c8a
Steve Konrad <skonrad@wingmail.net>
2018-08-03 22:31:55 +0200

    #5 | adjusted option index handling in js

--- bundle/Resources/public/js/scripts/fieldType/attribute/selection-defintion.js
+++ bundle/Resources/public/js/scripts/fieldType/attribute/selection-defintion.js
@@ -2,6 +2,7 @@
     const SELECTOR_ROOT_TABLE = '.intprogenhancedrelationlist-definition-attribute-edit .erl-table';
 
     const SELECTOR_OPTIONS_CONTAINER = '.erl-selection-attribute-edit';
+    const SELECTOR_LAST_OPTION_GROUP = '.input-group:last-child';
     const SELECTOR_LAST_OPTION = '.input-group:last-child input[data-value-path^="settings.options."]';
 
     document.querySelectorAll(SELECTOR_ROOT_TABLE).forEach(table => {
@@ -9,10 +10,11 @@
             const selectionContainer = event.target.closest(SELECTOR_OPTIONS_CONTAINER);
 
             const optionTemplate = JSON.parse(selectionContainer.getAttribute('data-option-template'));
-            selectionContainer.querySelector(SELECTOR_LAST_OPTION).removeEventListener('change', addOption);
+            const nextIndex = parseInt(selectionContainer.querySelector(SELECTOR_LAST_OPTION_GROUP).getAttribute('data-index')) + 1;
+            selectionContainer.querySelectorAll(SELECTOR_LAST_OPTION).forEach(option => {option.removeEventListener('change', addOption)});
 
-            selectionContainer.insertAdjacentHTML('beforeend', optionTemplate.replace(/%index%/g, ));
-            selectionContainer.querySelector(SELECTOR_LAST_OPTION).addEventListener('change', addOption);
+            selectionContainer.insertAdjacentHTML('beforeend', optionTemplate.replace(/%index%/g, nextIndex));
+            selectionContainer.querySelectorAll(SELECTOR_LAST_OPTION).forEach(option => {option.addEventListener('change', addOption)});
 
             table.dispatchEvent(new Event('change-fields'));
         };
